56b7c0266b76bd166e3aeace3c7c5c4b9af133d6
dinusha014 <chamoddinusha176@gmail.com>
2026-08-04 15:50:45 +0530

    Add API key constants

---
+++ ticket-service/src/main/java/com/eventsphere/ticketservice/config/ApiKeyConstants.java
@@ -0,0 +1,11 @@
+package com.eventsphere.ticketservice.config;
+
+public class ApiKeyConstants {
+
+    public static final String API_KEY_HEADER = "X-API-KEY";
+
+    public static final String API_KEY = "EVENTSPHERE2026";
+
+    private ApiKeyConstants() {
+    }
+}
